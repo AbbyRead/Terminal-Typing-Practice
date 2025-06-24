86a9a515d5b5faf62e919f1944da77952cb12b10
Abigail Read <AbbyReadMail@gmail.com>
2025-06-23 18:13:22 -0700

    Implement buffer creation

--- src/platform/os_mac.c
+++ src/platform/os_mac.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,16 +7,40 @@
 #include "options.h"
 #include "buffer.h"
 
-#define INITIAL_SIZE 4096
-
 enum Platform platform = MACOS;
 
-static char *create_buffer(FILE *source_text) {
-	char *buffer = malloc(INITIAL_SIZE);
-	char c;
-	while (!feof(source_text)) {
-		c = fgetc(source_text);
+#define INITIAL_SIZE 4096
+
+// Buffering logic: global state for reading input into buffer
+static size_t size = INITIAL_SIZE;
+static size_t i;
+static char *buffer = NULL;
+
+static void enlarge_buffer(void) {
+	size *= 2;
+	char *new_buffer = realloc(buffer, size);
+	if (!new_buffer) {
+		free(buffer);
+		perror("Failed to enlarge buffer");
+		exit(EXIT_FAILURE);
 	}
+	buffer = new_buffer;
+	new_buffer = NULL;
+}
+
+static char *create_buffer(FILE *source_text) {
+	buffer = malloc(INITIAL_SIZE);
+	if (!buffer) return NULL;
+
+	int c;
+	for (i = 0; (c = fgetc(source_text)) != EOF; ++i) {
+		buffer[i] = (char)c;
+
+		// size is max index exceeded by 1
+		if ((i + 1) == size) enlarge_buffer();
+	} 
+	if (!feof(source_text)) return NULL; // read error
+	buffer[i] = '\0'; // already enlarged by + 1 check
 	
 	return buffer;
 }
@@ -24,7 +49,6 @@ char *platform_read_clipboard(void) {
 	FILE *clipboard = popen("pbpaste", "r");
 	if (!clipboard) {
 		perror("Could not read from clipboard");
-		fclose(clipboard);
 		exit(EXIT_FAILURE);
 	} // opened clipboard
 
@@ -34,7 +58,7 @@ char *platform_read_clipboard(void) {
 		perror("Failed to copy clipboard to buffer");
 		pclose(clipboard);
 		exit(EXIT_FAILURE);
-	} else pclose(clipboard); // got buffer
+	} else pclose(clipboard); // got buffer; free clipboard
 
 	return buffer;
 }
