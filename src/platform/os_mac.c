4bf90f2c69d4b743edd3658fd6baa2b15d655916
Abigail Read <AbbyReadMail@gmail.com>
2025-07-01 03:38:57 -0700

    Reduce repetition by using a helper function

--- src/platform/os_mac.c
+++ src/platform/os_mac.c
@@ -15,36 +15,39 @@ void platform_initialize(void) {
 	setlocale(LC_CTYPE, "");
 }
 
+static text_buffer_t *read_from_stream(FILE *stream, const char *error_context) {
+	if (!stream) {
+		fprintf(stderr, "Unable to open stream: %s\n", error_context);
+		exit(EXIT_FAILURE);
+	}
+	text_buffer_t *buffer = read_stream_to_buffer(stream);
+	if (!buffer) {
+		fprintf(stderr, "Failed to read stream into buffer: %s\n", error_context);
+		exit(EXIT_FAILURE);
+	}
+	return buffer;
+}
+
 text_buffer_t *platform_read_clipboard(void) {
 	FILE *clipboard = popen("pbpaste", "rb");
 	if (!clipboard) return NULL;
-	text_buffer_t *buffer = read_stream_to_buffer(clipboard);
+	text_buffer_t = *buffer = read_from_stream(clipboard, "clipboard");
 	pclose(clipboard);
 	return buffer;
 }
 
 text_buffer_t *platform_read_stdin(void) {
 	if (isatty(fileno(stdin))) {
-		perror("No piped or redirected input detected");
+		fprintf(stderr, "No piped or redirected input detected.\n");
 		exit(EXIT_FAILURE);
 	}
-	FILE *fifo = stdin;
-	if (!fifo) {
-		perror("Unable to access stdin");
-		exit(EXIT_FAILURE);
-	}
-	text_buffer_t *buffer = read_stream_to_buffer(fifo);
-	if (!buffer) {
-		perror("Failed to copy buffer");
-		exit(EXIT_FAILURE);
-	}
-	return buffer;
+	return read_from_stream(stdin, "stdin");
 }
 
-text_buffer_t *platform_read_file(const char *filename) {
+text_buffer_t = *platform_read_file(const char *filename) {
 	FILE *file = fopen(filename, "rb");
 	if (!file) return NULL;
-	text_buffer_t *buffer = read_stream_to_buffer(file);
+	text_buffer_t *buffer = read_from_stream(file, filename);
 	fclose(file);
 	return buffer;
 }
